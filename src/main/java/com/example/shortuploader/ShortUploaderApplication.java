55dbbccefc42f633d5c8d0ab0da53a8a2d6c8b7f
xzz1p <olezhka.kirilov.97@mail.ru>
2024-01-30 19:33:27 +0300

    Initial commit

---
+++ src/main/java/com/example/shortuploader/ShortUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUploaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shortuploader/ShortUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUploaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
